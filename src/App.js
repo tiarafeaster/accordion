e6ff67aad328d93fce9934e2e784feb0a29a76b8
Tiara Feaster <tfeaster21@gmail.com>
2021-07-09 12:56:38 -0400

    added accordion feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import questions from "./data";
+import data from "./data";
+import SingleQuestion from "./Question";
 
 function App() {
   return (
     <main>
-      <h2>Hello out there</h2>
-      <h5>hi</h5>
+      <div className="container">
+        <h3>Questions and Answers about Login</h3>
+        <section className="info">
+          {questions.map((question) => {
+            return <SingleQuestion key={question.id} {...question} />;
+          })}
+        </section>
+      </div>
     </main>
   );
 }
